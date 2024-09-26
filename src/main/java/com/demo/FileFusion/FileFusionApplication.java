4144c3630489f86b2eaed876d9abae1e6b71bbec
Ashu Singh <chaudharyashu0822@gmail.com>
2024-09-26 16:14:27 +0530

    My first commit

---
+++ src/main/java/com/demo/FileFusion/FileFusionApplication.java
@@ -0,0 +1,13 @@
+package com.demo.FileFusion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileFusionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileFusionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/FileFusion/FileFusionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.FileFusion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileFusionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
